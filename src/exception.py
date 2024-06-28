c3288a1aad4266720ba9b3c1c308286b24d0638d
Akshatarya27 <akshatarya027@gmail.com>
2024-06-28 19:25:43 +0530

    data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -8,9 +9,16 @@ def error_message_details(error,error_detail:sys):
     return error_message
     
 
-    class CustomException(Exception):
+class CustomException(Exception):
         def __init__(self,error_message,error_detail:sys):
             super.__init__(error_message)
             self.error_message = error_message_details(error_message,error_detail=error_detail)
         def __str__(self):
             return self.error_message
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("DIVIDE BY ZERO")
+        raise CustomException(e,sys)
